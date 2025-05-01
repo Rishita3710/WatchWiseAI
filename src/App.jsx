3af1a1736a96d9bac55da50983986a3fdaf77bbf
rishita <rishita2019@gmail.com>
2025-05-01 11:40:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-emerald-800">Welcome to these</h1>
+    </>
+  )
+}
+
+export default App
