1489f13147cde8811a5b34c2a80a4e87b5a131d9
rishita <rishita2019@gmail.com>
2025-07-25 17:09:54 +0530

    Add new file and modified in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import {auth} from '../utils/firebase'
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {addUser, removeUser} from "../utils/userSlice"
+import { LOGO, UserLogo } from '../utils/constants';
 
 function Header(){
     const dispatch = useDispatch();
@@ -19,7 +20,7 @@ function Header(){
 
     }
         useEffect(()=>{
-      onAuthStateChanged(auth, (user) => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
   if (user) {
     const {uid, email, displayName, photoURL} = user;
     dispatch(addUser({uid:uid , email:email, displayName: displayName, photoURL:photoURL}));
@@ -31,12 +32,13 @@ function Header(){
 
   }
 });
+return () => unsubscribe();
     },[])
 
 
   return (
     <div className="absolute w-screen px-22 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img className="w-44" src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+      <img className="w-44" src={LOGO}
       alt="logo"
       />
       {user && (<div className="relative">
@@ -46,7 +48,7 @@ function Header(){
         >
           <img
             className="w-10 h-10 rounded-full"
-            src="https://cdn-icons-png.flaticon.com/512/9187/9187604.png"
+            src={UserLogo}
             alt="User Icon"
           />
           <svg
