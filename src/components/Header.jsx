e3bb1c529e5474d9137f8ddaaaf87b3bbe2bd611
rishita <rishita2019@gmail.com>
2025-07-25 16:54:59 +0530

    modified in src file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
-import { useState } from 'react';
-import { signOut } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import {auth} from '../utils/firebase'
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {addUser, removeUser} from "../utils/userSlice"
 
 function Header(){
+    const dispatch = useDispatch();
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const navigate = useNavigate();
     const user = useSelector((store)=> store.user);
     const handleSignOut = () =>{
       signOut(auth).then(() => {
-  navigate("/");
 }).catch((error) => {
   navigate("/error");
 });
 
     }
+        useEffect(()=>{
+      onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const {uid, email, displayName, photoURL} = user;
+    dispatch(addUser({uid:uid , email:email, displayName: displayName, photoURL:photoURL}));
+    navigate("/browse");
+
+  } else {
+    dispatch(removeUser);
+    navigate("/");
+
+  }
+});
+    },[])
+
 
   return (
     <div className="absolute w-screen px-22 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
